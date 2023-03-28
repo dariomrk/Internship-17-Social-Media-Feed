6ac8c69de51f48787ae887b1eec5e11ed2417916
dariomrk <dariomrkonjic1@gmail.com>
2023-03-28 16:37:34 +0200

    Adjust FeedPage view.

--- src/pages/FeedPage/FeedPage.jsx
+++ src/pages/FeedPage/FeedPage.jsx
@@ -3,6 +3,7 @@ import {
 } from '@mantine/core';
 import React, { useEffect, useState } from 'react';
 import { useDisclosure } from '@mantine/hooks';
+import { DoorExit, PencilPlus } from 'tabler-icons-react';
 import NewPostForm from '../../components/NewPostForm';
 import Post from '../../components/Post';
 import { getPosts } from '../../lib/content';
@@ -19,10 +20,10 @@ function FeedPage() {
       <Card shadow="sm" padding="lg" radius="md" withBorder>
         <Title order={2}>Actions</Title>
         <Group mt="md" position="apart" spacing="sm">
-          <Button color="red" variant="light">Log out</Button>
+          <Button leftIcon={<DoorExit />} color="red" variant="light">Log out</Button>
           <Group spacing="sm">
             <Button variant="light">My posts</Button>
-            <Button onClick={toggleNewPost}>Create new post</Button>
+            <Button leftIcon={<PencilPlus />} onClick={toggleNewPost}>Create new post</Button>
           </Group>
         </Group>
       </Card>
