821c452556d0a4056387676f2c88fb0ad6396b01
dariomrk <dariomrkonjic1@gmail.com>
2023-03-29 14:58:46 +0200

    Add 404 page.

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import {
+  Title, Text, Button, Group, Stack,
+} from '@mantine/core';
+import { useViewportSize } from '@mantine/hooks';
+import { useNavigate } from 'react-router-dom';
+import { Poo } from 'tabler-icons-react';
 
 function NotFoundPage() {
+  const { height } = useViewportSize();
+  const navigate = useNavigate();
   return (
-    <>
-    </>
+    <Stack justify="center" align="center" h={height}>
+      <Group>
+        <Poo size={42} />
+        <Title>Uh oh! 404</Title>
+      </Group>
+      <Text color="dimmed" size="lg" align="center">
+        In this reality, the URL you searched for does not exist.
+      </Text>
+      <Button
+        onClick={() => {
+          navigate('/');
+        }}
+        variant="subtle"
+        size="md"
+      >
+        Take me back
+      </Button>
+    </Stack>
   );
 }
 
